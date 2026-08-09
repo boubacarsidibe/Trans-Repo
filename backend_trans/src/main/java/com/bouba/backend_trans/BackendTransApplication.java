c68f9cf06142bc3cd7a55c3fb2be4a12c5dcb364
Boubacar Sidibe <sboubacar@ept.sn>
2026-08-09 22:51:11 +0000

    initialisation de la base de donnees

---
+++ backend_trans/src/main/java/com/bouba/backend_trans/BackendTransApplication.java
@@ -0,0 +1,13 @@
+package com.bouba.backend_trans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTransApplication.class, args);
+	}
+
+}
---
+++ backend_trans/src/test/java/com/bouba/backend_trans/BackendTransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouba.backend_trans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
